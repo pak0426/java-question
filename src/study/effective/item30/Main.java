b4f81b3a51dad095b9386ffd7f99bb695fe0cfe8
hmmini <pak4184@naver.com>
2024-10-01 12:00:00 +0900

    [이펙티브 자바] 아이템 30. 이왕이면 제네릭 메서드로 만들라

---
+++ src/study/effective/item30/Main.java
@@ -0,0 +1,52 @@
+package study.effective.item30;
+
+import java.util.*;
+import java.util.function.UnaryOperator;
+
+public class Main {
+    // 제네릭 메서드
+    public static <E> Set<E> union(Set<E> s1, Set<E> s2) {
+        Set<E> result = new HashSet<>(s1);
+        result.addAll(s2);
+        return result;
+    }
+
+    // 제네릭 싱글턴 팩터리 패턴
+    private static UnaryOperator<Object> IDENTITY_FN = (t) -> t;
+
+    @SuppressWarnings("unchecked")
+    public static <T> UnaryOperator<T> identityFunction() {
+        return (UnaryOperator<T>) IDENTITY_FN;
+    }
+
+    // 재귀적 타입 한정을 이용한 제네릭 메서드
+    public static <E extends Comparable<E>> E max(Collection<E> c) {
+        if (c.isEmpty())
+            throw new IllegalArgumentException("컬렉션이 비어 있습니다.");
+
+        E result = null;
+        for (E e : c)
+            if (result == null || e.compareTo(result) > 0)
+                result = Objects.requireNonNull(e);
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        // union 메서드 사용 예
+        Set<String> guys = Set.of("톰", "딕", "해리");
+        Set<String> stooges = Set.of("래리", "모에", "컬리");
+        Set<String> aflCio = union(guys, stooges);
+        System.out.println(aflCio);
+
+        // identityFunction 사용 예
+        String[] strings = { "삼베", "대마", "나일론" };
+        UnaryOperator<String> sameString = identityFunction();
+        for (String s : strings)
+            System.out.println(sameString.apply(s));
+
+        // max 메서드 사용 예
+        List<Integer> numbers = Arrays.asList(3, 1, 4, 1, 5, 9);
+        System.out.println(max(numbers));
+    }
+}
